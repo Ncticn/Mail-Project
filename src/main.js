6856427be0cf573c4d5ae43165407009d76faf5a
Necati CAN <nctiicn@gmail.com>
2024-08-29 08:31:14 +0300

    PrimeVue Installation Done and Added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-import './assets/tailwind.css'
+import "./assets/tailwind.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import PrimeVue from "primevue/config";
+import Aura from '@/presets/lara';
+import "primeicons/primeicons.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: Aura,
+  ripple: true,
+});
+
+app.mount("#app");
