fe2e5acda265bc64b7f2a58a3d25d65d0ff6545c
Necati CAN <nctiicn@gmail.com>
2024-11-16 20:19:00 +0300

    Confirm Dialog Delete Member Component Added and Edited.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup>
-import { RouterView } from "vue-router";
-import Footer from "@/components/footer/Footer.vue";
-import Navbar from "@/components/header/Navbar.vue";
+import Default from "./layouts/default.vue";
+
 </script>
 <template>
-  <header>
-    <Navbar />
-  </header>
-  <RouterView />
-  <footer>
-    <Footer />
-  </footer>
+  <Default/>
 </template>
